fd0872bdede98ba59ade74af8902108c71e79ce5
Emanuele Ballarin <emanuele@ballarin.cc>
2019-11-18 02:40:28 +0100

    Update PAPI installer

--- libpapi/papinstall.sh
+++ libpapi/papinstall.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 
+# Become location-aware
 export SELF_CALLDIR="$(pwd)"
 
+# Prepare
 rm -R -f ./papi_id
 mkdir ./papi_id
-
 cd ./papi_id
+
+# Get & Unpack
 wget http://icl.cs.utk.edu/projects/papi/downloads/papi-5.7.0.tar.gz
-
 tar -xvzf ./papi-5.7.0.tar.gz
-
 cd ./papi-5.7.0/src/
 
+# Patch
 patch -p1 < ../../../fix1.patch
 patch -p1 < ../../../fix2.patch
 
+# Configure
 export CFLAGS="-fPIC ${CFLAGS}"
 export CC=gcc
 ./configure \
@@ -22,9 +25,20 @@ export CC=gcc
   --with-static-lib=yes --with-shared-lib=yes \
   --with-perf-events
 
+# Build & Install
 make -j8
 sudo make install
 
+# Clean up
 cd "$SELF_CALLDIR"
 rm -R -f ./papi_id
 
+# Setup LD_LIBRARY_PATH
+sudo mkdir -p /etc/ld.so.conf.d/
+cd /etc/ld.so.conf.d/
+sudo touch /etc/ld.so.conf.d/locallib.conf
+sudo bash -c "echo \"/usr/local/lib/\" >> /etc/ld.so.conf.d/locallib.conf"
+
+# Update LD_LIBRARY_PATH systemwide
+cd "$SELF_CALLDIR"
+sudo ldconfig -v
